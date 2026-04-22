031e727f78d4161bdbe5575f65bd7232f31d6c67
minseo <p7548296@gmail.com>
2026-04-22 20:36:11 +0900

    t1,t2

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,11 +1,19 @@
 package com.ll.framework.ioc;
 
+import com.ll.domain.testPost.testPost.repository.TestPostRepository;
+import com.ll.domain.testPost.testPost.service.TestPostService;
+
 public class ApplicationContext {
     public ApplicationContext() {
 
     }
 
+    @SuppressWarnings("unchecked")
     public <T> T genBean(String beanName) {
-        return (T) null;
+        if ("testPostService".equals(beanName)) {
+            return (T) new TestPostService(new TestPostRepository());
+        }
+
+        return null;
     }
 }
